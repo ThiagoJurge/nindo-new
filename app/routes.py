dacd89557a7927f841ec0aac1565f7af50096056
ThiagoJurge <67167598+ThiagoJurge@users.noreply.github.com>
2025-02-14 14:03:43 -0300

    debugging

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,6 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/webhook', methods=['POST'])
 def webhook():
     data = request.get_json()
-    group_id = data['phone']  # Usando o group_id vindo do webhook
-    message_text = data['text']['message']
     
     # response_message = process_webhook(group_id, message_text)
     
